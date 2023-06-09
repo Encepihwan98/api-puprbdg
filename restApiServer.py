dcb3df8dc6a8ff059e1ea24e3b4475dcef530edb
Encep Ihwan <ihwanencep@gmail.com>
2023-06-10 05:31:13 +0700

    path

--- restApiServer.py
+++ restApiServer.py
@@ -39,7 +39,7 @@ chrome_options.add_argument('disable-notifications')
 chrome_options.add_argument('--disable-infobars')
 
 # driver = webdriver.Chrome(executable_path=path, options=chrome_options)
-driver = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path='/home/sibedaspbg/chromedriver/chromedrivers', chrome_options=chrome_options)
 driver.get(front)
 
 wait10 = WebDriverWait(driver, 10)
