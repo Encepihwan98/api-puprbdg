37b609d652a05e57d0ece90afd95b7988a59b802
Encep Ihwan <ihwanencep@gmail.com>
2023-06-09 17:12:03 +0700

    ubah driver

--- restApiServer.py
+++ restApiServer.py
@@ -38,7 +38,8 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('disable-notifications')
 chrome_options.add_argument('--disable-infobars')
 
-driver = webdriver.Chrome(executable_path=path, options=chrome_options)
+# driver = webdriver.Chrome(executable_path=path, options=chrome_options)
+driver = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
 driver.get(front)
 
 wait10 = WebDriverWait(driver, 10)
