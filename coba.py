ce1fbf8228e5a8a9157c8db1d77df44429c50a6d
sibedaspbg <sibedaspbg@sibedaspbg>
2023-06-14 04:02:46 +0000

    merge pull

--- coba.py
+++ coba.py
@@ -38,4 +38,4 @@ def simbg():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443)
+    app.run(debug=False)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from  restApiServer import app 
+
+if __name__ == "__main__":
+ app.run()
