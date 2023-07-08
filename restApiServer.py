52f9bfcc124a09f897bfc6f7adf555951b311f8a
Encep Ihwan <ihwanencep@gmail.com>
2023-07-08 20:09:09 +0700

    coba print page html

--- restApiServer.py
+++ restApiServer.py
@@ -103,6 +103,15 @@ paswd.send_keys("habibie11")
 btn_ijo = driver.find_element(By.XPATH, '//*[@id="frmLogin"]/div[3]/button[1]')
 btn_ijo.click()
 
+html = driver.page_source
+
+# Cetak halaman HTML
+print(html)
+
+# Menyimpan halaman HTML ke dalam file
+with open('halaman.html', 'w', encoding='utf-8') as file:
+    file.write(html)
+    
 driver.get(kons)
 
 cetak = wait10.until(
@@ -110,6 +119,7 @@ cetak = wait10.until(
         (By.XPATH, '//*[@id="frmListVerifikasi"]/div/div/div[3]/div/div/a')
     )
 )
+
 cetak.click()
 
 time.sleep(10)
