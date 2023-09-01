22a9429baa68650419815ff2b38b6c0ae029ddae
Encep Ihwan <ihwanencep@gmail.com>
2023-09-01 21:58:20 +0700

    revisi api lacak

--- apiLacak.py
+++ apiLacak.py
@@ -1,27 +1,84 @@
+# from flask import Blueprint, jsonify
+# import requests
+# import pandas as pd
+# from bs4 import BeautifulSoup
+
+# lacak_bp = Blueprint('lacak', __name__)
+
+# def scrape_data(nomor):
+#     s = requests.Session()
+
+#     home_url = 'https://simbg.pu.go.id/Informasi'
+#     home_resp = s.get(home_url, verify=False)
+#     soup = BeautifulSoup(home_resp.text, 'html.parser')
+#     csrf_test_name = soup.find('input', {'type': 'csrf_test_name'})
+
+#     url = f'https://simbg.pu.go.id/Informasi/Lacak/{nomor}?csrf_test_name={csrf_test_name}'
+#     resp = requests.get(url, verify=False)
+#     html = '<table> ' + resp.text + ' </table>'
+
+#     df = pd.read_html(html)[0].fillna('')
+#     df.columns = ['No.', 'Modul', 'Tanggal', 'Keterangan']
+#     return df.to_dict(orient='records')
+
+# @lacak_bp.route('/<string:nomor>', methods=['GET'])
+# def get_lacak_data(nomor):
+#     data = scrape_data(nomor)
+#     return jsonify(data)
+
+
 from flask import Blueprint, jsonify
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-lacak_bp = Blueprint('lacak', __name__)
+lacak_bp = Blueprint("lacak", __name__)
+
 
 def scrape_data(nomor):
-    s = requests.Session()
+    try:
+        s = requests.Session()
 
-    home_url = 'https://simbg.pu.go.id/Informasi'
-    home_resp = s.get(home_url, verify=False)
-    soup = BeautifulSoup(home_resp.text, 'html.parser')
-    csrf_test_name = soup.find('input', {'type': 'csrf_test_name'})
+        home_url = "https://simbg.pu.go.id/Informasi"
+        home_resp = s.get(home_url, verify=False)
+        home_resp.raise_for_status()  # Raise an exception if the request was not successful
 
-    url = f'https://simbg.pu.go.id/Informasi/Lacak/{nomor}?csrf_test_name={csrf_test_name}'
-    resp = requests.get(url, verify=False)
-    html = '<table> ' + resp.text + ' </table>'
+        soup = BeautifulSoup(home_resp.text, "html.parser")
+        csrf_test_name = soup.find("input", {"type": "csrf_test_name"})
 
-    df = pd.read_html(html)[0].fillna('')
-    df.columns = ['No.', 'Modul', 'Tanggal', 'Keterangan']
-    return df.to_dict(orient='records')
+        url = f"https://simbg.pu.go.id/Informasi/Lacak/{nomor}?csrf_test_name={csrf_test_name}"
+        resp = requests.get(url, verify=False)
+        resp.raise_for_status()
 
-@lacak_bp.route('/<string:nomor>', methods=['GET'])
+        html = "<table> " + resp.text + " </table>"
+
+        df = pd.read_html(html)[0].fillna("")
+        df.columns = ["No.", "Modul", "Tanggal", "Keterangan"]
+        return df.to_dict(orient="records")
+
+    except requests.exceptions.RequestException as e:
+        # Handle network-related errors
+        return {"error": "Network error occurred"}
+
+    except Exception as e:
+        # Handle other unexpected errors
+        return {"error": str(e)}
+
+
+@lacak_bp.route("/<string:nomor>", methods=["GET"])
 def get_lacak_data(nomor):
     data = scrape_data(nomor)
+
+    if "error" in data:
+        return jsonify(data), 500  # HTTP status code 500 for internal server error
+
     return jsonify(data)
+
+
+if __name__ == "__main__":
+    from flask import Flask
+
+    app = Flask(__name__)
+    app.register_blueprint(lacak_bp, url_prefix="/lacak")
+
+    app.run(debug=False)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from cobaTest import app
+from coba import app
 
 if __name__ == "__main__":
     app.run()
