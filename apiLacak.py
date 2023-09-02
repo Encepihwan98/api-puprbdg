f4c6fe09c03b5f97a855cee095851924b682d59e
Encep Ihwan <ihwanencep@gmail.com>
2023-09-02 07:47:30 +0700

    revisi coding blueprint

--- apiLacak.py
+++ apiLacak.py
@@ -1,85 +1,39 @@
-# from flask import Blueprint, jsonify
-# import requests
-# import pandas as pd
-# from bs4 import BeautifulSoup
-
-# lacak_bp = Blueprint('lacak', __name__)
-
-# def scrape_data(nomor):
-#     s = requests.Session()
-
-#     home_url = 'https://simbg.pu.go.id/Informasi'
-#     home_resp = s.get(home_url, verify=False)
-#     soup = BeautifulSoup(home_resp.text, 'html.parser')
-#     csrf_test_name = soup.find('input', {'type': 'csrf_test_name'})
-
-#     url = f'https://simbg.pu.go.id/Informasi/Lacak/{nomor}?csrf_test_name={csrf_test_name}'
-#     resp = requests.get(url, verify=False)
-#     html = '<table> ' + resp.text + ' </table>'
-
-#     df = pd.read_html(html)[0].fillna('')
-#     df.columns = ['No.', 'Modul', 'Tanggal', 'Keterangan']
-#     return df.to_dict(orient='records')
-
-# @lacak_bp.route('/<string:nomor>', methods=['GET'])
-# def get_lacak_data(nomor):
-#     data = scrape_data(nomor)
-#     return jsonify(data)
-
-
-from flask import Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 lacak_bp = Blueprint("lacak", __name__)
 
-
 def scrape_data(nomor):
-    try:
-        s = requests.Session()
+    s = requests.Session()
 
-        home_url = "https://simbg.pu.go.id/Informasi"
-        home_resp = s.get(home_url, verify=False)
-        home_resp.raise_for_status()  # Raise an exception if the request was not successful
+    home_url = 'https://simbg.pu.go.id/Informasi'
+    home_resp = s.get(home_url, verify=False)
+    soup = BeautifulSoup(home_resp.text, 'lxml')  # Menggunakan 'lxml' sebagai parser
+    csrf_test_name = soup.find('input', {'type': 'csrf_test_name'})
 
-        soup = BeautifulSoup(home_resp.text, "html.parser")
-        csrf_test_name = soup.find("input", {"type": "csrf_test_name"})
+    url = f'https://simbg.pu.go.id/Informasi/Lacak/{nomor}?csrf_test_name={csrf_test_name}'
+    resp = requests.get(url, verify=False)
+    html = '<table> ' + resp.text + ' </table>'
 
-        url = f"https://simbg.pu.go.id/Informasi/Lacak/{nomor}?csrf_test_name={csrf_test_name}"
-        resp = requests.get(url, verify=False)
-        resp.raise_for_status()
+    soup = BeautifulSoup(html, 'lxml')  # Menggunakan 'lxml' sebagai parser
+    table = soup.find('table')
+    df = pd.read_html(str(table))[0].fillna('')
 
-        html = "<table> " + resp.text + " </table>"
+    df.columns = ['No.', 'Modul', 'Tanggal', 'Keterangan']
+    return df.to_dict(orient='records')
 
-        df = pd.read_html(html)[0].fillna('')
-        df.columns = ['No.', 'Modul', 'Tanggal', 'Keterangan']
-
-        return df.to_dict(orient="records")
-
-    except requests.exceptions.RequestException as e:
-        # Handle network-related errors
-        return {"error": "Network error occurred"}
-
-    except Exception as e:
-        # Handle other unexpected errors
-        return {"error": str(e)}
-
-
-@lacak_bp.route("/<string:nomor>", methods=["GET"])
+@lacak_bp.route('/<string:nomor>', methods=['GET'])
 def get_lacak_data(nomor):
     data = scrape_data(nomor)
-
-    if "error" in data:
-        return jsonify(data), 500  # HTTP status code 500 for internal server error
-
     return jsonify(data)
 
+app.register_blueprint(lacak_bp, url_prefix='/api/lacak')
 
-if __name__ == "__main__":
-    from flask import Flask
-
-    app = Flask(__name__)
-    app.register_blueprint(lacak_bp, url_prefix="/lacak")
-
+if __name__ == '__main__':
     app.run(debug=False)
--- rekapPBG.py
+++ rekapPBG.py
@@ -1,30 +1,35 @@
 from flask import Blueprint, jsonify
 import gspread
 import pandas as pd
-from datetime import date, timedelta
 from flask_cors import CORS
 
 rekap_bp = Blueprint('rekap', __name__)
+CORS(rekap_bp)  # Enable CORS for this Blueprint
 
 # Load the Google Sheets service account credentials
-sa = gspread.service_account(filename='sibedaspbg-logbook-cab4b99bdcae.json')
-
-# Open the spreadsheet
-sp = sa.open('rekap pbg')
-sh = sp.worksheet('Data')
+try:
+    gc = gspread.service_account(filename='sibedaspbg-logbook-cab4b99bdcae.json')
+    sh = gc.open('rekap pbg').worksheet('Data')
+except Exception as e:
+    print(f"Error loading Google Sheets: {str(e)}")
+    sh = None
 
 # Fetch data from the spreadsheet and process it
 def fetch_data():
-    data = sh.get_all_values()[1:]
-    columns = sh.get_all_values()[0]
-    logb = pd.DataFrame(data, columns=columns)
-    tahuns = []
-    for tahun, year in zip(logb['TAHUN TERBIT'], logb['Tahun Berjalan']):
-        tahuns.append(max(tahun, year))
-    logb['Tahun'] = tahuns
-    logb = logb[logb['Tahun'] != '']
-    logb.drop(columns=['2/8', 'TAHUN TERBIT', 'Tahun Berjalan'], inplace=True)
-    return logb
+    if sh:
+        try:
+            data = sh.get_all_values()[1:]
+            columns = sh.get_all_values()[0]
+            logbook = pd.DataFrame(data, columns=columns)
+            logbook['Tahun'] = logbook[['TAHUN TERBIT', 'Tahun Berjalan']].max(axis=1)
+            logbook = logbook.drop(columns=['2/8', 'TAHUN TERBIT', 'Tahun Berjalan'])
+            logbook = logbook[logbook['Tahun'] != '']
+            return logbook
+        except Exception as e:
+            print(f"Error fetching data from Google Sheets: {str(e)}")
+            return pd.DataFrame()
+    else:
+        return pd.DataFrame()
 
 @rekap_bp.route('/', methods=['GET'])
 def get_data():
